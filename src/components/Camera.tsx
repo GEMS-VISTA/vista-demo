0ac868c6b47d2894537d27fbaa8bd33fd98e5b1a
aakinw <aakinw21@terpmail.umd.edu>
2025-05-07 06:04:49 -0400

    Added Django backend communication

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -2,7 +2,7 @@ const Camera = () => {
   return (
     <div>
       <h2>Live Feed</h2>
-      <img src="http://127.0.0.1:8000/vid_feed/" alt="Camera Feed" />
+      <img src="https://vista-server.ddns.net/vid_feed/" alt="Camera Feed" />
     </div>
   );
 };
